3410d43ffb6c8ed0854d4c5b623fc107f748d6af
yoonhero <yoonhero06@naver.com>
2022-04-01 21:18:13 +0900

    feat: Send Message

--- go.mod
+++ go.mod
@@ -1,3 +1,9 @@
 module github.com/yoonhero/yoonhero_slackbot
 
 go 1.17
+
+require (
+	github.com/gorilla/websocket v1.5.0 // indirect
+	github.com/joho/godotenv v1.4.0 // indirect
+	github.com/slack-go/slack v0.10.2 // indirect
+)
--- main.go
+++ main.go
@@ -1,7 +1,28 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+
+	"github.com/joho/godotenv"
+	"github.com/slack-go/slack"
+)
 
 func main() {
-	fmt.Println("Hello World")
+	err := godotenv.Load(".env")
+
+	api := slack.New(os.Getenv("SLACK_BOT_TOKEN"))
+
+	channelID, timestamp, err := api.PostMessage(
+		"hi",
+		slack.MsgOptionText("Hello World", false),
+		slack.MsgOptionAsUser(false),
+	)
+
+	if err != nil {
+		fmt.Printf("%s\n", err)
+		return
+	}
+
+	fmt.Printf("Message sent successfully to channel %s at %s\n", channelID, timestamp)
 }
