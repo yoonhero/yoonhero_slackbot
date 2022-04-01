3edabf524b2e7e98376ae17ceedd43d993817cf9
yoonhero <yoonhero06@naver.com>
2022-04-01 21:56:17 +0900

    feat: Websocket Slack Bot

--- main.go
+++ main.go
@@ -1,22 +1,70 @@
 package main
 
 import (
-	"fmt"
+	"context"
+	"log"
 	"os"
 
 	"github.com/joho/godotenv"
 	"github.com/slack-go/slack"
-	"github.com/yoonhero/yoonhero_slackbot/messages"
+	"github.com/slack-go/slack/slackevents"
+	"github.com/slack-go/slack/socketmode"
 )
 
 func main() {
 	_ = godotenv.Load(".env")
 
-	api := slack.New(os.Getenv("SLACK_BOT_TOKEN"))
+	token := os.Getenv("SLACK_BOT_TOKEN")
+	appToken := os.Getenv("SLACK_APP_TOKEN")
 
-	err := messages.SendMsg("HI", api)
+	client := slack.New(token, slack.OptionDebug(true), slack.OptionAppLevelToken(appToken))
 
-	if err != nil {
-		fmt.Printf("[ERROR]: %s\n", err)
-	}
+	socketClient := socketmode.New(
+		client,
+		socketmode.OptionDebug(true),
+		socketmode.OptionLog(log.New(os.Stdout, "socketmode: ", log.Lshortfile|log.LstdFlags)),
+	)
+
+	// Create a context that can be used to cancel goroutine
+	ctx, cancel := context.WithCancel(context.Background())
+	// Make this cancel called properly in a real program , graceful shutdown etc
+	defer cancel()
+
+	go func(ctx context.Context, client *slack.Client, socketClient *socketmode.Client) {
+		// Create a for loop that selects either the context cancellation or the events incomming
+		for {
+			select {
+			// inscase context cancel is called exit the goroutine
+			case <-ctx.Done():
+				log.Println("Shutting down socketmode listener")
+				return
+			case event := <-socketClient.Events:
+				// We have a new Events, let's type switch the event
+				// Add more use cases here if you want to listen to other events.
+				switch event.Type {
+				// handle EventAPI events
+				case socketmode.EventTypeEventsAPI:
+					// The Event sent on the channel is not the same as the EventAPI events so we need to type cast it
+					eventsAPIEvent, ok := event.Data.(slackevents.EventsAPIEvent)
+					if !ok {
+						log.Printf("Could not type cast the event to the EventsAPIEvent: %v\n", event)
+						continue
+					}
+					// We need to send an Acknowledge to the slack server
+					socketClient.Ack(*event.Request)
+					// Now we have an Events API event, but this event type can in turn be many types, so we actually need another type switch
+					log.Println(eventsAPIEvent)
+				}
+
+			}
+		}
+	}(ctx, client, socketClient)
+
+	socketClient.Run()
+
+	// err := messages.SendMsg("HI", api)
+
+	// if err != nil {
+	//	fmt.Printf("[ERROR]: %s\n", err)
+	//}
 }
